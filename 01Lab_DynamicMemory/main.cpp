f2d4c9a8c29a1100934b635fd81cb69406638f01
Shereen Khoja <shereen@pacificu.edu>
2021-02-12 10:19:10 -0800

    Add project files.

---
+++ 01Lab_DynamicMemory/main.cpp
@@ -0,0 +1,68 @@
+//***************************************************************************
+// File name:  main.cpp
+// Author:     Chadd Williams & Shereen Khoja
+// Date:       2/12/2021
+// Class:      CS485
+// Assignment: Dynamic Memory Lab
+// Purpose:    Practice using raw pointers and smart pointers
+//***************************************************************************
+
+
+#include <iostream>
+#include "PacString.h"
+#include "vld.h"
+
+int main ()
+{
+
+  //TODO:
+  // Install and set up Visual Leak Detector using the instructions on Moodle
+  // Write out all of the functions for PacString.cpp
+  // Test them by uncommenting BLOCK 1
+  // Uncomment and complete BLOCK 2
+  // Uncomment and complete BLOCK 3
+  // Submit answers to Moodle quiz
+
+  //BLOCK 1
+ // PacString s1 = "Hello!";
+
+ // std::cout << "s1:" << s1 << std::endl;
+
+ // PacString s2 ("World");
+ // std::cout << "s2: " << s2 << std::endl;
+
+	//PacString s3;
+
+ // s3 = s1 + s2;
+
+ // std::cout << "s3: " << s3 << std::endl;
+
+ // s3 = s3 + "!!";
+
+ // std::cout << "s3: " << s3 << std::endl;
+
+
+
+  // BLOCK 2:
+  // PacString *pcDynString;
+
+  // Dynamically allocate the PacString object using
+  // pcDynString using a raw pointer and assign "CS485" to pcDynString.
+
+  // Display pcDynString.
+
+  // Concatenate " is the best!" on to
+  // pcDynString
+
+  // Display pcDynString.
+
+  // Deallocate the object pcDynString 
+
+
+  // BLOCK 3:
+  // Do the same thing that you did in Block 3 but using smart pointers
+
+  return EXIT_SUCCESS;
+}
+
+
